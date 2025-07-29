2bc631aae2dd07a35276f2ccae0dd1b7e4147712
olatejulian <julian.lf.olate@gmail.com>
2025-07-29 13:58:29 -0300

    feat: :sparkles: add rename keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,6 +62,7 @@ return {
 
             vim.lsp.enable("ruff")
 
+            vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { desc = "LSP Rename Symbol" })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
