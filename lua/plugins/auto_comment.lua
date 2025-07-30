5a2ffe5015e6f16e5dcc897bc162fb4bb02fe283
olatejulian <julian.lf.olate@gmail.com>
2025-07-30 20:31:37 -0300

    refactor: :recycle: change keymaps to match with ghostty encoding

--- lua/plugins/auto_comment.lua
+++ lua/plugins/auto_comment.lua
@@ -4,8 +4,8 @@ return {
         config = function()
             require("Comment").setup()
 
-            vim.keymap.set("n", "<C-_>", "gcc", { remap = true, desc = "Toggle comment line" })
-            vim.keymap.set("v", "<C-_>", "gc", { remap = true, desc = "Toggle comment selection" })
+            vim.keymap.set("n", "<C-/>", "gcc", { remap = true, desc = "Toggle comment line" })
+            vim.keymap.set("v", "<C-/>", "gc", { remap = true, desc = "Toggle comment selection" })
         end,
     },
 }
