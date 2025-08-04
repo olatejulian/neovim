da590404942320fbc9fe4851dad9558b11ba5abe
olatejulian <julian.lf.olate@gmail.com>
2025-08-04 02:10:42 -0300

    refactor: :recycle: change reveal to toggle behavior

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     config = function()
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
     end,
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
     lazy = false,
