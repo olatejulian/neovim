4ebc9cc0db4a0af8bdda3d056ba5fdacef3a4b76
olatejulian <julian.lf.olate@gmail.com>
2025-08-14 20:00:11 -0300

    feat: :sparkles: add keymaps for splitted window movement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,11 @@ vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
 -- move lines
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
--- vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down in insert" })
--- vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up in insert" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
 
+-- switch between windows
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
