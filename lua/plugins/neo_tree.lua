f2d650283579b8cb1bdf7178d80e7713c557739a
olatejulian <julian.lf.olate@gmail.com>
2025-08-16 09:12:25 -0300

    refactor: :recycle: add silent to keymap

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -1,7 +1,9 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     config = function()
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {
+            silent = true,
+        })
     end,
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
     lazy = false,
