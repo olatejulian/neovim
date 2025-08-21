cf6db2aea61656060e704b6a20870f4f7cd32d54
olatejulian <julian.lf.olate@gmail.com>
2025-08-20 21:29:45 -0300

    :sparkles: set shfmt

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -8,7 +8,10 @@ return {
                 none_ls.builtins.diagnostics.mypy,
                 none_ls.builtins.diagnostics.textidote,
                 none_ls.builtins.formatting.latexindent,
-                none_ls.builtins.formatting.prettier,
+                none_ls.builtins.formatting.prettier.with({
+                    prefer_local = "node_modules/.bin",
+                }),
+                none_ls.builtins.formatting.shfmt,
                 none_ls.builtins.formatting.stylua,
                 require("none-ls.diagnostics.eslint"),
                 require("none-ls.diagnostics.ruff"),
@@ -17,7 +20,7 @@ return {
             },
         })
 
-        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+        vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, { desc = "[F]ormat [T]ext" })
     end,
     dependencies = {
         "nvimtools/none-ls-extras.nvim",
