a5022af2224769d0d6b1f82f4d1a272c907f3e7e
olatejulian <julian.lf.olate@gmail.com>
2025-08-25 21:40:43 -0300

    refactor: :recycle: fix wrong keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
             local builtin = require('telescope.builtin')
 
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
-            vim.keymap.set('n', '<leader>ft', builtin.live_grep, { desc = '[F]ind [C]ode' })
+            vim.keymap.set('n', '<leader>fc', builtin.live_grep, { desc = '[F]ind [C]ode' })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers ' })
             vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = '[T]elescope [H]elp' })
         end,
