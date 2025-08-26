e1470eedf10c7a2e1c755a33f49f22806730f871
olatejulian <julian.lf.olate@gmail.com>
2025-08-25 21:52:40 -0300

    feat: :sparkles: add conform plugin for formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+    "stevearc/conform.nvim",
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "ruff" },
+                typescript = { "prettier" },
+            },
+        })
+
+        vim.keymap.set(
+            "n",
+            "<leader>ft",
+            conform.format,
+            { noremap = true, silent = true, desc = "[F]ormat [T]ext" }
+        )
+    end,
+}
